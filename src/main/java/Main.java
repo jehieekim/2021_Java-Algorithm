5a4730132fb1926eb72d6a2e143b032a275690a0
jeheekim <jehee.kim@gmail.com>
2021-10-10 13:39:06 +0900

    1.String(문자열)-2.대소문자 변환-1차작성

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,16 @@
 import java.util.*;
 
 public class Main {
-    public int solution(String str, char t){
-            int answer = 0;
-            str = str.toUpperCase();
-            t = Character.toUpperCase(t); //괄호안에 t
-            for(int i = 0; i < str.length(); i++){
-                if(str.charAt(i) == t) answer++;
-            }
+    public String solution(String str, char t){
+        String answer = "";
 
         return answer;
     }
     public static void main(String[] args) {
         Main T = new Main();
         Scanner in = new Scanner(System.in);
-        String str = in.next();
-        char c = in.next().charAt(0);
-        System.out.print(T.solution(str,c));
+        String input = in.next();
+        System.out.print(T.solution(input));
 
     }
 }
