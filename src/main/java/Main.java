00eedfbfb6ceb13373ca70bb23930105ed52adc9
jeheekim <jehee.kim@gmail.com>
2021-10-10 13:27:31 +0900

    1.String(문자열)-1.문자찾기-한번더 풀어봄

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,10 +4,11 @@ public class Main {
     public int solution(String str, char t){
             int answer = 0;
             str = str.toUpperCase();
-            t = Character.toUpperCase(t);
+            t = Character.toUpperCase(t); //괄호안에 t
             for(int i = 0; i < str.length(); i++){
-                if(str.charAt(i)==t) answer++;
+                if(str.charAt(i) == t) answer++;
             }
+
         return answer;
     }
     public static void main(String[] args) {
@@ -15,6 +16,7 @@ public class Main {
         Scanner in = new Scanner(System.in);
         String str = in.next();
         char c = in.next().charAt(0);
-        System.out.print(T.solution(str, c));
+        System.out.print(T.solution(str,c));
+
     }
 }
