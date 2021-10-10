50e99234a862b61b037478b83c34477ea0763546
jeheekim <jehee.kim@gmail.com>
2021-10-10 13:07:55 +0900

    1.String(문자열)-1.문자찾기

---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    public int solution(String str, char t){
+            int answer = 0;
+            str = str.toUpperCase();
+            t = Character.toUpperCase(t);
+            for(int i = 0; i < str.length(); i++){
+                if(str.charAt(i)==t) answer++;
+            }
+        return answer;
+    }
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner in = new Scanner(System.in);
+        String str = in.next();
+        char c = in.next().charAt(0);
+        System.out.print(T.solution(str, c));
+    }
+}
