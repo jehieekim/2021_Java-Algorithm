1f6bae722c8db6f4973f4cb7bd85e6e5ac463f48
jeheekim <jehee.kim@gmail.com>
2021-10-10 15:24:47 +0900

    1.String(문자열)-2.대소문자 변환-다시작성1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,18 +3,21 @@ import java.util.*;
 public class Main {
     public String solution(String str){
         String answer = "";
-        for(char x : str.toCharArray()) {
-            if(Character.isLowerCase(x)) answer += Character.toUpperCase(x);
-            else answer += Character.toLowerCase(x);
+        for(char x : str.toCharArray()){
+         if(Character.isLowerCase(x)) answer += Character.toUpperCase(x);
+         else answer += Character.toLowerCase(x);
+
         }
         return answer;
+
     }
 
     public static void main(String[] args) {
-        Main T = new Main();
-        Scanner in = new Scanner(System.in);
-        String str = in.next();
-        System.out.print(T.solution(str));
+      Main T = new Main();
+      Scanner in = new Scanner(System.in);
+            System.out.println("문자를 입력하세요");
+      String input = in.nextLine();
+            System.out.print(T.solution(input));
 
     }
 }
